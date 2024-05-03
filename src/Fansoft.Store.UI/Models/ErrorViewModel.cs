fea04a8bfacea5108194c42f1baf5748916dc588
Fabiano Nalin <fnalin@msn.com>
2024-05-03 10:59:14 +0200

    add repo

---
+++ src/Fansoft.Store.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Fansoft.Store.UI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
